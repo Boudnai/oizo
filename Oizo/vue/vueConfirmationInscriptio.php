6c7fcdb4882c963fd2c83a9a10509e44216b2f64
Boudnai <156792201+Boudnai@users.noreply.github.com>
2025-04-03 09:39:36 +0200

    Delete Oizo directory

--- Oizo/vue/vueConfirmationInscriptio.php
+++
@@ -1,9 +0,0 @@
-<main>
-    <section class="bg relative">
-        <div class="conf-insc">
-            <p>Inscription effectuée</p>
-            <p>Veuillez vous <a href="./?action=connexion">connecter</a></p>
-        </div>
-        
-    </section>
-</main>
