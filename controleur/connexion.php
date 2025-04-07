cfcbec260aa9bf961ed8027a135585384b975902
Boudnai <cbounadijj@gmail.com>
2025-04-07 21:07:22 +0200

    changements connexion

--- controleur/connexion.php
+++ controleur/connexion.php
@@ -1,4 +1,15 @@
 <?php
+if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
+    $racine = "..";
+}
+
+// Inclusion des fichiers nécessaires
+include_once "$racine/modele/authentification.inc.php";
+include_once "$racine/modele/bd.utilisateur.inc.php";
+
+// Récupérer les messages éventuels
+$messages = recupererMessages();
+
 // Formulaire de connexion ou traitement de la connexion
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
     // Afficher le formulaire de connexion
@@ -10,6 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
     // Traiter le formulaire de connexion
     if (!isset($_POST["Email"]) || !isset($_POST["mdp"])) {
         gererErreur("Veuillez remplir tous les champs", "?action=connexion");
+        exit;
     }
     
     $mailU = $_POST["Email"];
@@ -20,10 +32,12 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
     
     if (isLoggedOn()) {
         // Connexion réussie
-        gererSucces("Connexion réussie", "?action=profil");
+        gererSucces("Connexion réussie", "?action=accueil");
+        exit;
     } else {
         // Échec de la connexion
         gererErreur("Email ou mot de passe incorrect", "?action=connexion");
+        exit;
     }
 }
 ?>
